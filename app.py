b136b64a0ba29c48067daaabc5bb8db003383b1f
Swastik Biswas <swastikbiswas962@gmail.com>
2024-09-01 02:01:33 +0530

    main file (no running code yet)

---
+++ app.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+import threading
+
+from modules import routine, apps
+from modules import logging
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Shutting Down")
